958fafcecf1f60614efa3664afcea025d181c054
Daniel Anguiano <anguianoda58@hotmail.com>
2025-02-08 13:51:58 -0600

    Creacion de datos

---
+++ app.js
@@ -0,0 +1,9 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Challenge amigo secreto
+//elaborado por Daniel Anguiano Mujica
+//definimos las funcionalidades:
+
+//agrega nombres:los ususarios escribiran el nombre de un amigo en un campo de texto y lo agregaran a una lista visible al hacer clic en adicionar
+//valida entrada: si el campo esta vacio muestra alerta pididendo nombre valido
+// visualiza la lista: los nombres ingresados apareceran en una lista debajo del campo de entrada
+//sorteo aleatoreo: al hacer clic en el boton "sortear amigo", se seleccionara aleatoriamente un nombre de la lista y se mostrara en la pagina
