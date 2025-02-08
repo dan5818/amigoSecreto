94ca70f6376e01313078251e960c3e96115ba093
Daniel Anguiano <anguianoda58@hotmail.com>
2025-02-08 14:14:13 -0600

    agregamos el array

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 //definimos las funcionalidades:
 
 //agrega nombres:los ususarios escribiran el nombre de un amigo en un campo de texto y lo agregaran a una lista visible al hacer clic en adicionar
+let amigos = [];//creamos el array como indica el trello
+
 //valida entrada: si el campo esta vacio muestra alerta pididendo nombre valido
 // visualiza la lista: los nombres ingresados apareceran en una lista debajo del campo de entrada
 //sorteo aleatoreo: al hacer clic en el boton "sortear amigo", se seleccionara aleatoriamente un nombre de la lista y se mostrara en la pagina
